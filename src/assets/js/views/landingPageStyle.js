283ff520c8b225019654a136367399f468d459a9
Ifeoluwa Oluwayelu <46842207+Oluwayelu@users.noreply.github.com>
2021-04-07 22:21:24 +0100

    Update landingPageStyle.js

--- src/assets/js/views/landingPageStyle.js
+++ src/assets/js/views/landingPageStyle.js
@@ -45,7 +45,7 @@ const landingPageStyle = {
       "0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2)"
   },
   logo: {
-    width: "150px",
+    width: "100px",
   }
 };
 
